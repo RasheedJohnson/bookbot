dd9c07ecb1f2d1cffe79d19ab018759b7fcdf8bd
Rasheed Johnson <rasheedjohnson@tutanota.com>
2024-05-01 09:35:41 +0200

    remove old implementation and sys.argv requirement. add feature to print char frquency in text file

--- main.py
+++ main.py
@@ -1,20 +1,14 @@
 import sys
 
 def main():
-    path_to_file = "books/frankenstein.txt"
+    # path_to_file = "books/frankenstein.txt"
+    path_to_file = "books/text.txt"
     file_contents = get_text(path_to_file)
     word_list = get_word_count(file_contents)
     word_count = len(word_list)
     
-    
-    if sys.argv[1] == None:
-        user_input == "arg"
-    else:
-        user_input = sys.argv[1]
-        
-    characters = char_count(user_input, file_contents)
-    collection = f"Word count: {word_count}\ntext character count: {characters}"
-    print(collection)
+    char_count(file_contents)
+
 
 def get_text(path):
     with open(path) as f:
@@ -24,17 +18,26 @@ def get_word_count(text):
     words = text.split()
     return words
 
-def char_count(received, words):
+def char_count(file_text):
     # counts char appearance in words for each char in received
+    chars_match = "abcdefghijklmnopqrstuvwxyz0123456789"
+    symbols = ["'", '"', "\\n", ",", ".", " "]
     count = {}
-    for i in received:
-      i_count = 0
-      for j in words:
-        if j.lower() == i:
-          i_count += 1
-      count[i] = i_count
-    
-    return count
+    new_percentage = 0
+    for i in file_text:
+        i = i.lower()
+        i_count = 0
+        for j in file_text:
+          if j.lower() == i:
+            i_count += 1
+        count[i] = i_count
+        percentage = round((len(count) / (len(chars_match)+len(symbols))) * 100)
+        if percentage > new_percentage:
+          new_percentage = percentage
+          print(f"{percentage}%", end="\r")
+    print("100%", end="\r")
+    print(count)
+    # return count
 
 
 if __name__ == "__main__":
